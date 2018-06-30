3f68a455325d2c0b14f4a00c1b122d125c9a452a
trenthudy <trenthudy@gmail.com>
2018-06-29 22:10:09 -0400

    allow for nullable description field from github api

--- app/src/main/java/io/hudepohl/mvvm/data/repo/model/Repo.kt
+++ app/src/main/java/io/hudepohl/mvvm/data/repo/model/Repo.kt
@@ -7,7 +7,7 @@ import com.squareup.moshi.JsonClass
 data class Repo(
         @Json(name = "id") val id: Int,
         @Json(name = "name") val name: String,
-        @Json(name = "description") val description: String = "",
+        @Json(name = "description") val description: String? = "",
         @Json(name = "owner") val owner: RepoOwner,
         @Json(name = "private") val private: Boolean = false,
         @Json(name = "archived") val archived: Boolean = false,
