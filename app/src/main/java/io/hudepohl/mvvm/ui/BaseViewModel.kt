439dfee78466e7ced9f0b31789158f45954571cb
trenthudy <trenthudy@gmail.com>
2018-09-30 10:09:02 -0400

    minor updates

--- app/src/main/java/io/hudepohl/mvvm/ui/BaseViewModel.kt
+++ app/src/main/java/io/hudepohl/mvvm/ui/BaseViewModel.kt
@@ -2,21 +2,29 @@ package io.hudepohl.mvvm.ui
 
 import android.app.Application
 import android.arch.lifecycle.AndroidViewModel
+import android.arch.lifecycle.MutableLiveData
 import android.databinding.Observable
 import android.databinding.PropertyChangeRegistry
 import android.support.annotation.CallSuper
+import io.hudepohl.mvvm.app.BaseApplication
+import io.reactivex.Single
 import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.disposables.Disposable
 import java.util.concurrent.atomic.AtomicBoolean
 
 
 abstract class BaseViewModel(app: Application) : AndroidViewModel(app), Observable {
 
     private var initialized: AtomicBoolean = AtomicBoolean(false)
-    protected val disposables: CompositeDisposable = CompositeDisposable()
+    private val registry: PropertyChangeRegistry = PropertyChangeRegistry()
+    private val disposables: CompositeDisposable = CompositeDisposable()
+    val errorMessages: MutableLiveData<String> = MutableLiveData()
 
     fun onResume() = if (initialized.compareAndSet(false, true)) setup() else {}
 
     abstract fun setup()
+
+    protected fun getApp() = getApplication<BaseApplication>()
 
     @CallSuper
     override fun onCleared() {
@@ -24,13 +32,16 @@ abstract class BaseViewModel(app: Application) : AndroidViewModel(app), Observab
         super.onCleared()
     }
 
-    // databinding helpers
-    private val registry: PropertyChangeRegistry by lazy { PropertyChangeRegistry() }
     override fun addOnPropertyChangedCallback(callback: Observable.OnPropertyChangedCallback?) = synchronized(this) { registry.add(callback) }
     override fun removeOnPropertyChangedCallback(callback: Observable.OnPropertyChangedCallback?) = synchronized(this) { registry.remove(callback) }
     fun notifyChange() = synchronized(this) { registry.notifyCallbacks(this, 0, null) }
     fun notifyPropertyChanged(fieldId: Int) = synchronized(this) { registry.notifyCallbacks(this, fieldId, null) }
 
-    // rx helpers
-    // TODO: Add RxJava helper functions...
+    protected fun <T> Single<T>.listenOnViewModel(
+            onSuccess: (T) -> Unit
+    ): Disposable = this.subscribe(onSuccess).also { disposables.add(it) }
+    protected fun <T> Single<T>.listenOnViewModel(
+            onSuccess: (T) -> Unit,
+            onError: (Throwable) -> Unit
+    ): Disposable = this.subscribe(onSuccess, onError).also { disposables.add(it) }
 }
--- app/src/main/java/io/hudepohl/mvvm/ui/main/MainViewModel.kt
+++ app/src/main/java/io/hudepohl/mvvm/ui/main/MainViewModel.kt
@@ -1,13 +1,39 @@
 package io.hudepohl.mvvm.ui.main
 
 import android.app.Application
+import android.arch.lifecycle.MutableLiveData
+import io.hudepohl.mvvm.R
+import io.hudepohl.mvvm.data.beatles.BeatlesService
+import io.hudepohl.mvvm.data.beatles.model.BeatleAlbum
 import io.hudepohl.mvvm.ui.BaseViewModel
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
 import javax.inject.Inject
 
 
-class MainViewModel @Inject constructor(app: Application) : BaseViewModel(app) {
+class MainViewModel @Inject constructor(
+        app: Application,
+        private val beatlesService: BeatlesService
+) : BaseViewModel(app) {
+
+    val albumData = MutableLiveData<List<BeatleAlbum>>()
 
     override fun setup() {
 
+        beatlesService
+                .getBeatleAlbums()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .listenOnViewModel(
+                        { albums ->
+                            albumData.value = albums
+                        },
+                        { error ->
+                            errorMessages.value = if (error.message != null) {
+                                error.message
+                            } else {
+                                getApp().getString(R.string.error_generic)
+                            }
+                        })
     }
 }
